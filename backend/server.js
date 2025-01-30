b390e749516f0180271681fda67bac8344ee67e4
varenya-ramayanam <ramayanamvarenya@gmail.com>
2025-01-30 18:13:30 +0530

    env backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 const placesRoutes = require("./routes/places");
 
 const app = express();
@@ -11,5 +12,6 @@ app.use(express.json());
 // Use the places API route
 app.use("/api", placesRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  
